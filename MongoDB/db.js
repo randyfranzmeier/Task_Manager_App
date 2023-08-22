19822d87124094464b7277d4ebe044dcd6a20903
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-08-22 14:09:09 -0500

    Installing mongodb and mongoose api

---
+++ MongoDB/db.js
@@ -0,0 +1,4 @@
+const mongoose = require(mongoose)
+const uri = "mongodb+srv://randyfranzmeier:<Romanrich44>@cluster0.portob0.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(uri).then(()=> console.log("connection successful")).catch((err) => console.log(err))
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('../MongoDB/db')
 const port = 3000
 
 app.get("/api/getAllTasks", (req, res) => {
