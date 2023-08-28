98e97583f749333843a3c8a2eaa90f36471249dd
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-08-28 15:50:25 -0500

    Fixed errors, created task component

--- Frontend/taskApp/src/app/app.component.spec.ts
+++ Frontend/taskApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tmApp'`, () => {
+  it(`should have as title 'taskApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tmApp');
+    expect(app.title).toEqual('taskApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tmApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('taskApp app is running!');
   });
 });
---
+++ Frontend/taskApp/src/app/task.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'app-task',
+    template: '<p>Task Component<p>'
+})
+export class Task {}
