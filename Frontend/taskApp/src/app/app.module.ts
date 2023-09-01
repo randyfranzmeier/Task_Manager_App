5b1353c88ab203f7687e21d7e75b497e9d937043
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-08-31 20:36:27 -0500

    Adding task creater component

--- Frontend/taskApp/src/app/app.module.ts
+++ Frontend/taskApp/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { Task } from './task/task.component';
 import { FormsModule } from '@angular/forms';
 import { Layout } from './layout/layout.component';
 import { SearchComponent } from './search/search.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Task,
     Layout,
-    SearchComponent
+    SearchComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
