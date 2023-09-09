eae704dc4d7144785d3c00594b18295bdc6df4e7
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-09-09 08:10:33 -0500

    enhancing style and logic for tasks

--- Frontend/taskApp/src/app/create-task/create-task.component.ts
+++ Frontend/taskApp/src/app/create-task/create-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-create-task',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-task.component.css']
 })
 export class CreateTaskComponent {
+//get data, make sure it is valid, send it to task comp, call fetch, display on screen
+titleControl = ''
+desControl = ''
 
+taskData(event: Event) {
+   event.preventDefault();
+   console.log(this.titleControl)
+   console.log(this.desControl)
 }
+}
+
